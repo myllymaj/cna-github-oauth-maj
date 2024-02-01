dd0cd6c757a3d59fe119b9c0889a107d50415c85
Fredrik Welander <welandfr@arcada.fi>
2024-02-01 20:25:29 +0200

    Tidy up

--- index.js
+++ index.js
@@ -6,11 +6,9 @@ const port = process.env.PORT || 3000
 const app = express()
 
 app.get("/", (req, res) => {
-    return res.send(`
-        <h1>Hello!</h1>
-    `)
+    return res.send(`<h1>Hello!</h1>`)
 });
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}`)
 });
